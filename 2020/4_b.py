730f35894c86de326871a74116f36dbeb540e00c
Johan Nyholm <hans.johan.nyholm@gmail.com>
2020-12-04 23:26:42 +0100

    Refactor 2020 4 b

--- 2020/4_b.py
+++ 2020/4_b.py
@@ -5,69 +5,62 @@ import common
 
 
 def parse_input(file_name):
-    passports_raw = []
-    passport_raw = ''
-    for line in common.get_input_lines(file_name):
-        if not line:
-            passports_raw.append(passport_raw)
-            passport_raw = ''
-            continue
-        if passport_raw:
-            passport_raw += ' ' + line
-        else:
-            passport_raw = line
-    passports_raw.append(passport_raw)
-
-    passports = []
-    for passport_raw in passports_raw:
-        passport = {}
-        for kw_pair in passport_raw.split(' '):
-            key, value = kw_pair.split(':')
-            passport[key] = value
-        passports.append(passport)
-
-    return passports
-
-
-def _parse_int(val, default=None):
-    try:
-        return int(val)
-    except ValueError:
-        return default
+    return [
+        {
+            kv[0]: kv[1]
+            for kv
+            in re.findall(r'(?:(\S+)\:(\S+))', passport_raw)
+        } for passport_raw in common.get_input(file_name).split('\n\n')
+    ]
 
 
 def _validate_int(value, min, max):
-    return min <= _parse_int(value, 0) <= max
+    return min <= int(value) <= max
+
+
+def _validate_inches(value, min, max):
+    match = re.search(r'^(\d+)in$', value)
+    return match is not None and _validate_int(match.group(1), min, max)
+
+
+def _validate_centimeters(value, min, max):
+    match = re.search(r'^(\d+)cm$', value)
+    return match is not None and _validate_int(match.group(1), min, max)
 
 
 def _validate_hgt(value):
-    match = re.search(r'^(\d+)in$', value)
-    if match is not None:
-        return _validate_int(match.group(1), 59, 76)
-    match = re.search(r'^(\d+)cm$', value)
-    if match is not None:
-        return _validate_int(match.group(1), 150, 193)
-    return False
+    return (
+        _validate_inches(value, 59, 76)
+        or _validate_centimeters(value, 150, 193)
+    )
 
 
 def _validate_ecl(value):
-    return value in set(['amb','blu', 'brn','gry', 'grn','hzl','oth'])
+    return value in set(['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth'])
 
 
 def _validate_regex(value, regex):
     return re.match(regex, value) is not None
 
 
-def solve(passports, required_fields, validators):
-    num_valid = 0
-    for passport in passports:
-        passport_keys = set(passport.keys())
-        if required_fields.difference(passport_keys):
-            continue
-        if not all(validators[key](value) for key, value in passport.items() if key in validators):
-            continue
-        num_valid += 1
-    return num_valid
+def _validate_passport(passport, required_fields, field_validators):
+    return (
+        not required_fields.difference(set(passport.keys()))
+        and all(
+            field_validators[key](value)
+            for key, value in passport.items() if key in field_validators
+        )
+    )
+
+
+def solve(passports, required_fields, field_validators):
+    return sum(
+        _validate_passport(
+            passport,
+            required_fields,
+            field_validators
+        ) for passport in passports
+    )
 
 
 required_fields = set([
@@ -80,7 +73,7 @@ required_fields = set([
     'pid',
     # 'cid',
 ])
-validators = {
+field_validators = {
     'byr': partial(_validate_int, min=1920, max=2002),
     'iyr': partial(_validate_int, min=2010, max=2020),
     'eyr': partial(_validate_int, min=2020, max=2030),
@@ -92,5 +85,5 @@ validators = {
 }
 file_name = '4.txt'
 passports = parse_input(file_name)
-solution = solve(passports, required_fields, validators)
+solution = solve(passports, required_fields, field_validators)
 print(solution)
