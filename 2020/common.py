87bd47329c8d62a5844d16324c93a5134701ff9b
Johan Nyholm <hans.johan.nyholm@gmail.com>
2020-12-09 22:06:08 +0100

    Add timer context manager

--- 2020/common.py
+++ 2020/common.py
@@ -1,3 +1,6 @@
+import time
+
+
 def get_input(file_name):
     with open(f'input/{file_name}', 'r') as f:
         return f.read()
@@ -9,3 +12,28 @@ def get_input_lines(file_name):
 
 def get_int_input(file_name):
     return [int(v) for v in get_input_lines(file_name)]
+
+
+class Timer:
+    def __init__(self, name='execution', verbose=True, ms=True):
+        self.t0 = None
+        self.name = name
+        self.verbose = verbose
+        self.ms = ms
+        self.ns_delta = -1
+
+    def print(self):
+        delta = self.ns_delta
+        unit = 'ms' if self.ms else 'ns'
+        if self.ms:
+            delta/=10**6
+        print(f'{self.name} took {delta} {unit}')
+
+    def __enter__(self):
+        self.t0 = time.perf_counter_ns()
+
+    def __exit__(self, *args):
+        self.t1 = time.perf_counter_ns()
+        self.ns_delta = self.t1 - self.t0
+        if self.verbose:
+            self.print()
