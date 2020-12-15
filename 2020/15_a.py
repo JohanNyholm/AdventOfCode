90a74d8b518ead9a1712763880e417878521ca29
Johan Nyholm <hans.johan.nyholm@gmail.com>
2020-12-15 23:00:03 +0100

    Solve 2020 15

---
+++ 2020/15_a.py
@@ -0,0 +1,24 @@
+import common
+
+
+def parse_input(file_name):
+    return [int(v) for v in common.get_input(file_name).split(',')]
+
+
+def solve(numbers, turns):
+    last_spoken_lookup = {n:i+1 for i, n in enumerate(numbers[:-1])}
+    prev_number = numbers[-1]
+    for turn in range(len(numbers), turns):
+        new_number = 0
+        if prev_number in last_spoken_lookup:
+            new_number = turn - last_spoken_lookup[prev_number]
+        last_spoken_lookup[prev_number] = turn
+        prev_number = new_number
+    return prev_number
+
+
+file_name = '15.txt'
+numbers = parse_input(file_name)
+with common.Timer():
+    solution = solve(numbers, 2020)
+print(solution)
